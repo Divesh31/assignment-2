15104b31402bcd2058f49038a7d590cf58fa3cd0
Divesh31 <diveshpal31@gmail.com>
2025-08-07 12:23:22 +0530

    Add files via upload

---
+++ assignment 2.py
@@ -0,0 +1,14 @@
+#TASK 1
+
+number=int(input('Enter a Number :'))
+if number% 2:
+    print(f' {number} is an Even Nunber')
+else:
+    print(f' {number} is an Odd Number')
+
+# TASK 2
+sum =0
+for i in range (1,51):
+    sum =sum +i
+print('The sum of number form 1 to 51 is:',sum)
+
